73b797f2b99a9cd0cd0ecdcc9d0c3cf8fc5d94c1
kminoda <koji.minoda@tier4.jp>
2024-08-17 14:00:19 +0900

    second commit
    Signed-off-by: kminoda <koji.minoda@tier4.jp>

--- src/stream_petr_node.cpp
+++ src/stream_petr_node.cpp
@@ -23,6 +23,132 @@ namespace tensorrt_stream_petr
 StreamPetrNode::StreamPetrNode(const rclcpp::NodeOptions & node_options)
 : Node("tensorrt_stream_petr", node_options)
 {
-  RCLCPP_INFO(get_logger(), "Finish initialize TensorRT model");
+  using std::placeholders::_1;
+  throw std::runtime_error("hoge");
+
+  RCLCPP_INFO(get_logger(), "HEY");
+  // Initialize parameters
+  const std::string onnx_backbone_path = declare_parameter<std::string>("onnx_backbone_path");
+  const std::string onnx_head_path = declare_parameter<std::string>("onnx_head_path");
+  const std::string precision_backbone = declare_parameter<std::string>("precision_backbone");
+  const std::string precision_head = declare_parameter<std::string>("precision_head");
+
+  // Subscriber
+  sub_image_ = create_subscription<Image>(
+    "~/input/image_raw", 10, std::bind(&StreamPetrNode::on_image, this, _1));
+
+  RCLCPP_INFO(get_logger(), "nvinfer: %d.%d.%d\n", NV_TENSORRT_MAJOR, NV_TENSORRT_MINOR, NV_TENSORRT_PATCH);
+  // cudaSetDevice(0);
+
+  // auto runtime_deleter = [](IRuntime *runtime) { /* runtime->destroy(); */ };
+  // std::unique_ptr<IRuntime, decltype(runtime_deleter)> runtime{createInferRuntime(gLogger), runtime_deleter};
+  // backbone_ = std::make_unique<SubNetwork>(onnx_backbone_path, runtime.get());
+  // pts_head_ = std::make_unique<SubNetwork>(onnx_head_path, runtime.get());
+
+  // cudaStreamCreate(&stream_);
+  // backbone_->EnableCudaGraph(stream_);
+  // pts_head_->EnableCudaGraph(stream_);
+
+  // mem_.mem_stream = stream_;
+  // mem_.pre_buf = (float*)pts_head_->bindings["pre_memory_timestamp"]->ptr;
+  // mem_.post_buf = (float*)pts_head_->bindings["post_memory_timestamp"]->ptr;
+
+  // // events for measurement
+  // dur_backbone_ = std::make_unique<Duration>("backbone");
+  // dur_ptshead_ = std::make_unique<Duration>("ptshead");
+
+  // const std::filesystem::path data_dir{"data"};
+
+  // int n_frames = 0;
+  // for (auto const& dir_entry : std::filesystem::directory_iterator{data_dir}) {
+  //   n_frames ++;
+  // }
+  // RCLCPP_INFO(get_logger(), "Total frames: %d\n", n_frames);
+
+  // for (int i = 0; i < n_frames; ++i) {
+  //   inference(i);
+  // }
 }
+
+void StreamPetrNode::on_image(const Image & msg){
+  (void)msg;
+}
+
+void StreamPetrNode::inference(const int f) {
+  // load data
+  char buf[5] = {0};
+  sprintf(buf, "%04d", f);
+  std::string frame_dir = "./data/" + std::string(buf) + "/";
+  std::cout << frame_dir << std::endl;
+  // img
+  backbone_->bindings["img"]->load(frame_dir + "img.bin");
+
+  dur_backbone_->MarkBegin(stream_);
+  // inference
+  backbone_->Enqueue(stream_);
+  dur_backbone_->MarkEnd(stream_);
+
+  cudaMemcpyAsync(
+    pts_head_->bindings["x"]->ptr,
+    backbone_->bindings["img_feats"]->ptr, 
+    backbone_->bindings["img_feats"]->nbytes(), 
+    cudaMemcpyDeviceToDevice, stream_);
+
+  pts_head_->bindings["pos_embed"]->load(frame_dir + "pos_embed.bin");
+  pts_head_->bindings["cone"]->load(frame_dir + "cone.bin");
+  
+  // load double timestamp from file
+  double stamp_current = 0.0;
+  char stamp_buf[8];
+  std::ifstream file_(frame_dir + "data_timestamp.bin", std::ios::binary);
+  file_.read(stamp_buf, sizeof(double));
+  stamp_current = reinterpret_cast<double*>(stamp_buf)[0];
+  std::cout << "stamp: " << stamp_current << std::endl;
+
+  if( is_first_frame_ ) {
+    // binary is stored as double
+    pts_head_->bindings["pre_memory_timestamp"]->load<double, float>(frame_dir + "prev_memory_timestamp.bin");
+
+    // start from dumped values
+    pts_head_->bindings["pre_memory_embedding"]->load(frame_dir + "init_memory_embedding.bin");
+    pts_head_->bindings["pre_memory_reference_point"]->load(frame_dir + "init_memory_reference_point.bin");
+    pts_head_->bindings["pre_memory_egopose"]->load(frame_dir + "init_memory_egopose.bin");
+    pts_head_->bindings["pre_memory_velo"]->load(frame_dir + "init_memory_velo.bin");
+
+    is_first_frame_ = false;
+  } else {
+    // update timestamp
+    mem_.StepPre(stamp_current);
+  }        
+
+  pts_head_->bindings["data_ego_pose"]->load(frame_dir + "data_ego_pose.bin");
+  pts_head_->bindings["data_ego_pose_inv"]->load(frame_dir + "data_ego_pose_inv.bin");
+
+  // inference
+  dur_ptshead_->MarkBegin(stream_);
+  pts_head_->Enqueue(stream_);
+  dur_ptshead_->MarkEnd(stream_);
+  mem_.StepPost(stamp_current);
+
+  // copy mem_post to mem_pre for next round
+  pts_head_->bindings["pre_memory_embedding"]->mov(pts_head_->bindings["post_memory_embedding"], stream_);
+  pts_head_->bindings["pre_memory_reference_point"]->mov(pts_head_->bindings["post_memory_reference_point"], stream_);
+  pts_head_->bindings["pre_memory_egopose"]->mov(pts_head_->bindings["post_memory_egopose"], stream_);
+  pts_head_->bindings["pre_memory_velo"]->mov(pts_head_->bindings["post_memory_velo"], stream_);
+  
+  cudaStreamSynchronize(stream_);
+
+  std::cout << "backbone: " << dur_backbone_->Elapsed() 
+            << ", ptshead: " << dur_ptshead_->Elapsed() 
+            << std::endl;
+
+  // validate(pts_head, frame_dir, "all_bbox_preds"); 
+  // validate(pts_head, frame_dir, "all_cls_scores"); 
+
+  // dump preds and labels
+  pts_head_->bindings["all_bbox_preds"]->save(frame_dir + "/all_bbox_preds_trt.bin");
+  pts_head_->bindings["all_cls_scores"]->save(frame_dir + "/all_cls_scores_trt.bin");
+}
+
+
 }  // namespace tensorrt_stream_petr
--- src/stream_petr_node.hpp
+++ src/stream_petr_node.hpp
@@ -27,19 +27,284 @@
 #include <rclcpp/rclcpp.hpp>
 #include <string>
 
+#include <sensor_msgs/msg/image.hpp>
+
+// From NVIDIA/DL4AGX
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <memory>
+#include <chrono>
+#include <unordered_map>
+
+#include <cuda_fp16.h>
+#include <cuda_runtime_api.h>
+#include <NvInferRuntime.h>
+#include "memory.cuh"
+// From NVIDIA/DL4AGX
+
 namespace tensorrt_stream_petr
 {
+// Taken from NVIDIA/DL4AGX
+#define LASTERR()   { \
+  auto code = cudaGetLastError(); \
+  if( code != cudaSuccess) { \
+    std::cout << cudaGetErrorString(code) << std::endl; \
+  } \
+}
+
+using namespace nvinfer1;
+
+class Logger : public ILogger {
+public:
+void log(Severity severity, const char* msg) noexcept override {
+  // Only print error messages
+  if (severity == Severity::kERROR) {
+    std::cerr << msg << std::endl;
+  }
+}
+};
+
+inline unsigned int getElementSize(nvinfer1::DataType t) {
+  switch (t) {
+    case nvinfer1::DataType::kINT32: return 4;
+    case nvinfer1::DataType::kFLOAT: return 4;
+    case nvinfer1::DataType::kHALF: return 2;
+    case nvinfer1::DataType::kINT8: return 1;
+  }
+  throw std::runtime_error("Invalid DataType.");
+  return 0;
+}
+
+struct Tensor {
+  std::string name;
+  void* ptr;
+  Dims dim;
+  int32_t volume = 1;
+  DataType dtype;
+  TensorIOMode iomode;
+
+  Tensor(std::string name, Dims dim, DataType dtype): 
+    name(name), dim(dim), dtype(dtype) 
+  {
+    if( dim.nbDims == 0 ) {
+      volume = 0;
+    } else {
+      volume = 1;
+      for(int i=0; i<dim.nbDims; i++) {
+        volume *= dim.d[i];
+      }
+    }
+    cudaMalloc(&ptr, volume * getElementSize(dtype));
+  }
+
+  int32_t nbytes() {
+    return volume * getElementSize(dtype);
+  }
+
+  void mov(std::shared_ptr<Tensor> other, cudaStream_t stream) {
+    // copy from 'other'
+    cudaMemcpyAsync(
+        ptr, other->ptr, 
+        nbytes(), 
+        cudaMemcpyHostToDevice,
+        stream);
+  }
+
+  template<class Htype=float, class Dtype=float>
+  void load(std::string fname) {
+    size_t hsize = volume * sizeof(Htype);
+    size_t dsize = volume * getElementSize(dtype);
+    std::vector<char> b1(hsize);
+    std::vector<char> b2(dsize);
+    std::ifstream file_(fname, std::ios::binary);
+    if( file_.fail() ) {
+      std::cerr << fname << " missing!" << std::endl;
+      return;
+    }
+    file_.read(b1.data(), hsize);
+    Htype* hbuffer = reinterpret_cast<Htype*>(b1.data());
+    Dtype* dbuffer = reinterpret_cast<Dtype*>(b2.data());
+    // in some cases we want to load from different dtype
+    for( int i=0; i<volume; i++ ) {
+      dbuffer[i] = (Dtype)hbuffer[i];
+    }
+
+    cudaMemcpy(ptr, b2.data(), dsize, cudaMemcpyHostToDevice);
+  }
+
+  template<class Htype=float, class Dtype=float>
+  void save(std::string fname) {
+    size_t hsize = volume * sizeof(Htype);
+    size_t dsize = volume * getElementSize(dtype);
+    std::vector<char> b1(hsize);
+    std::vector<char> b2(dsize);
+    std::ofstream file_(fname, std::ios::binary);
+    if( file_.fail() ) {
+      std::cerr << fname << " can't open!" << std::endl;
+      return;
+    }
+    // file_.read(b1.data(), hsize);
+    Htype* hbuffer = reinterpret_cast<Htype*>(b1.data());
+    Dtype* dbuffer = reinterpret_cast<Dtype*>(b2.data());
+    cudaMemcpy(b2.data(), ptr, dsize, cudaMemcpyDeviceToHost);
+    // in some cases we want to load from different dtype
+    for( int i=0; i<volume; i++ ) {
+      hbuffer[i] = (Htype)dbuffer[i];
+    }
+    file_.write(b2.data(), hsize);
+    file_.close();
+  }
+
+  std::vector<float> cpu() {
+    std::vector<float> buffer(volume);
+    cudaMemcpy(buffer.data(), ptr, volume * sizeof(float), cudaMemcpyDeviceToHost);
+    return buffer;
+  }
+
+  std::vector<char> load_ref(std::string fname) {
+    size_t bsize = volume * sizeof(float);
+    std::vector<char> buffer(bsize);
+    std::ifstream file_(fname, std::ios::binary);
+    file_.read(buffer.data(), bsize);
+    return buffer;
+  }
+}; // struct Tensor
+
+std::ostream& operator<<(std::ostream& os, Tensor& t) {
+  os << "[" << (int)(t.iomode) << "] ";
+  os << t.name << ", [";
+  
+  for( int nd=0; nd<t.dim.nbDims; nd++ ) {
+    if( nd == 0 ) {
+      os << t.dim.d[nd];
+    } else {
+      os << ", " << t.dim.d[nd];
+    }
+  }
+  std::cout << "]";
+  std::cout << ", type = " << int(t.dtype);
+  return os;
+}
+
+Logger gLogger;
+
+class SubNetwork {
+  ICudaEngine* engine;
+  IExecutionContext* context; 
+public:
+  std::unordered_map<std::string, std::shared_ptr<Tensor>> bindings;
+  bool use_cuda_graph = false;
+  cudaGraph_t graph;
+  cudaGraphExec_t graph_exec;
+
+  SubNetwork(std::string engine_path, IRuntime* runtime) {
+    std::ifstream engine_file(engine_path, std::ios::binary);
+    if (!engine_file) {
+      throw std::runtime_error("Error opening engine file: " + engine_path);
+    }
+    engine_file.seekg(0, engine_file.end);
+    long int fsize = engine_file.tellg();
+    engine_file.seekg(0, engine_file.beg);
+
+    // Read the engine file into a buffer
+    std::vector<char> engineData(fsize);
+
+    engine_file.read(engineData.data(), fsize);
+    engine = runtime->deserializeCudaEngine(engineData.data(), fsize);
+    context = engine->createExecutionContext(); 
+
+    int nb = engine->getNbIOTensors();  
+
+    for( int n=0; n<nb; n++ ) {
+      std::string name = engine->getIOTensorName(n);
+      Dims d = engine->getTensorShape(name.c_str());            
+      DataType dtype = engine->getTensorDataType(name.c_str());
+      bindings[name] = std::make_shared<Tensor>(name, d, dtype);
+      bindings[name]->iomode = engine->getTensorIOMode(name.c_str());
+      std::cout << *(bindings[name]) << std::endl;
+      context->setTensorAddress(name.c_str(), bindings[name]->ptr);
+    }
+  }
+
+  void Enqueue(cudaStream_t stream) {
+    if( this->use_cuda_graph ) {
+      cudaGraphLaunch(graph_exec, stream);
+    } else {
+      context->enqueueV3(stream);
+    }  
+  }
+
+  ~SubNetwork() {
+  }
+
+  void EnableCudaGraph(cudaStream_t stream) {        
+    // run first time to avoid allocation
+    this->Enqueue(stream);
+    cudaStreamSynchronize(stream);
+
+    cudaStreamBeginCapture(stream, cudaStreamCaptureModeGlobal);
+    this->Enqueue(stream);
+    cudaStreamEndCapture(stream, &graph);
+    this->use_cuda_graph = true;
+#if CUDART_VERSION < 12000
+    cudaGraphInstantiate(&graph_exec, graph, NULL, NULL, 0);
+#else
+    cudaGraphInstantiate(&graph_exec, graph, 0);
+#endif
+  }
+}; // class SubNetwork
+
+class Duration {
+  // stat
+  std::vector<float> stats;
+  cudaEvent_t b, e;
+  std::string m_name;
+public:
+  Duration(std::string name): m_name(name) {
+    cudaEventCreate(&b);
+    cudaEventCreate(&e);
+  }
+
+  void MarkBegin(cudaStream_t s) {
+    cudaEventRecord(b, s);
+  }
+
+  void MarkEnd(cudaStream_t s) {
+    cudaEventRecord(e, s);
+  }
+
+  float Elapsed() {
+    float val;
+    cudaEventElapsedTime(&val, b, e);
+    stats.push_back(val);
+    return val;
+  }
+}; // class 
+// Taken from NVIDIA/DL4AGX
+
 class StreamPetrNode : public rclcpp::Node
 {
+  using Image = sensor_msgs::msg::Image;
+
 public:
   explicit StreamPetrNode(const rclcpp::NodeOptions & node_options);
 
-  struct NodeParam
-  {
-    std::string onnx_backbone_path{};
-    std::string onnx_head_path{};
-    std::string precision{};
-  };
+private:
+  void inference(const int f);
+  void on_image(const Image & msg);
+
+  rclcpp::Subscription<Image>::SharedPtr sub_image_;
+
+  std::unique_ptr<SubNetwork> backbone_;
+  std::unique_ptr<SubNetwork> pts_head_;
+  std::unique_ptr<Duration> dur_backbone_;
+  std::unique_ptr<Duration> dur_ptshead_;
+  Memory mem_;
+  cudaStream_t stream_; 
+  bool is_first_frame_ = false;
 };
 
 }  // namespace tensorrt_stream_petr
